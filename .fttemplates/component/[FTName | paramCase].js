d37bc3b5ed799bcd538086db2869168d1e9017c7
Amir.H Ebrahimi <amirhosseinebrahimi77@gmail.com>
2021-05-15 10:42:29 +0430

    fix: Typo in commments

--- .fttemplates/component/[FTName | paramCase].js
+++ .fttemplates/component/[FTName | paramCase].js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 // style
 import useStyle from './[FTName | paramCase].style'
-// localiztion
+// localization
 import t from './[FTName | paramCase].local'
 
 export default function <FTName | pascalcase>(props) {
